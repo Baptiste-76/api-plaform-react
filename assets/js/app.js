891d339a69d67495be08f889862ba4bf53186dff
Baptiste Bidaux <baptistelise@orange.fr>
2019-12-18 18:18:02 +0100

    Mise en place de l'authentification

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch, withRouter } from "react-router-dom";
 import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
+import AuthContext from "./contexts/AuthContext";
 import CustomersPage from './pages/CustomersPage';
 import HomePage from './pages/HomePage';
 import InvoicesPage from './pages/InvoicesPage';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import LoginPage from './pages/LoginPage';
+import AuthAPI from './services/authAPI';
 
-// any CSS you require will output into a single css file (app.css in this case)
+// On apporte le CSS personnalisé
 require('../css/app.css');
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+AuthAPI.setup();
 
 const App = () => {
-    return (
-        <HashRouter>
-            <Navbar />
 
-            <main className="container pt-5">
-                <Switch>
-                    <Route path="/invoices" component={InvoicesPage} />
-                    <Route path="/customers" component={CustomersPage} />
-                    <Route path="/" component={HomePage} />
-                </Switch>
-            </main>
-        </HashRouter>   
+    const [isAuthenticated, setIsAuthenticated] = useState(AuthAPI.isAuthenticated());
+
+    const NavbarWithRouter = withRouter(Navbar);
+
+    return (
+        <AuthContext.Provider value = {{
+            isAuthenticated: isAuthenticated,
+            setIsAuthenticated: setIsAuthenticated
+        }}>
+            <HashRouter>
+                <NavbarWithRouter />
+
+                <main className="container pt-5">
+                    <Switch>
+                        <Route path="/login" component={LoginPage} />} />
+                        <PrivateRoute path="/invoices" component={InvoicesPage} />
+                        <PrivateRoute path="/customers" component = { CustomersPage } />
+                        <Route path="/" component={HomePage} />
+                    </Switch>
+                </main>
+            </HashRouter>   
+        </AuthContext.Provider>
     );
 };
 
---
+++ assets/js/services/authAPI.js
@@ -0,0 +1,82 @@
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+import customersAPI from "./customersAPI";
+
+/**
+ * Requête HTTP d'authentification et stockage du token dans le LocalStorage et sur Axios
+ * @param {object} credentials 
+ */
+function authenticate(credentials) {
+    return axios
+        .post("http://localhost:8000/api/login_check", credentials)
+        .then(response => response.data.token)
+        .then(token => {
+            
+            // On stocke le token dans le LocalStorage
+            window.localStorage.setItem("authToken", token);
+        
+            // On prévient axios qu'on a maintenant un header par défaut sur toutes nos futures requêtes HTTP
+            setAxiosToken(token);
+
+            customersAPI.findAll().then(data => console.log(data));
+         });
+}
+
+/**
+ * Déconnexion (suppression du token du LocalStorage et sur Axios)
+ */
+function logout() {
+    window;localStorage.removeItem("authToken");
+    delete axios.defaults.headers["Authorization"];
+    customersAPI.findAll().then(data => console.log(data));
+}
+
+/**
+ * Positionne le token JWT sur Axios
+ * @param {string} token Le token JWT
+ */
+function setAxiosToken(token) {
+    axios.defaults.headers["Authorization"] = "Bearer " + token;
+}
+
+/**
+ * Mise en place lors du chargement de l'application
+ */
+function setup() {
+    // 1. On voit si on a un token
+    const token = window.localStorage.getItem("authToken");
+
+    // 2. On voit s'il est toujours valide
+    if (token) {
+        const jwtData = jwtDecode(token);
+        if (jwtData.exp * 1000 > new Date().getTime()) {
+            setAxiosToken(token);
+        } 
+    } 
+}
+
+/**
+ * Permet de savoir si un utilisateur est authentifié ou pas
+ * @returns boolean
+ */
+function isAuthenticated() {
+    // 1. On voit si on a un token
+    const token = window.localStorage.getItem("authToken");
+
+    // 2. On voit s'il est toujours valide
+    if (token) {
+        const jwtData = jwtDecode(token);
+        if (jwtData.exp * 1000 > new Date().getTime()) {
+            return true;
+        } 
+        return false;
+    } 
+    return false;
+}
+
+export default {
+    authenticate: authenticate,
+    logout: logout,
+    setup: setup,
+    isAuthenticated: isAuthenticated
+};
